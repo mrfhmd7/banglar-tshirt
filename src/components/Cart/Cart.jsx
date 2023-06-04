b6986cb40b4d8314adf5896ccfd040f9f4d724c2
mrfhmd7 <mrf.hmd7@gmail.com>
2023-06-05 01:13:32 +0600

    conditional rendering if else added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Cart = ({cart, handleRemoveFromCart}) => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
+     let message;
+     if (cart.length === 0){
+          message = <p>Please add some products</p>
+          }
+     else {
+          message = <div>
+               <h2>Boroloxx!!!</h2>
+               <p>Thanks for westing money!!</p>
+          </div>
+     }
      return (
           <div>
                <h2>Order summary: {cart.length}</h2>
+               {message}
                {
                     cart.map(tshirt => <p
                          key={tshirt._id}>
@@ -16,3 +27,8 @@ const Cart = ({cart, handleRemoveFromCart}) => {
 };
 
 export default Cart;
+
+/* 
+*Conditional rendering
+1. use if else to set a variable that will contain a element, components
+*/
