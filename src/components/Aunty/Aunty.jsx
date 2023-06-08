37a725af98d160596a77a14312835b10b4d18e0e
mrfhmd7 <mrf.hmd7@gmail.com>
2023-06-08 14:00:56 +0600

    myself, brother, sister and cousin added

--- src/components/Aunty/Aunty.jsx
+++ src/components/Aunty/Aunty.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Cousin from '../Cousin/Cousin';
 
 const Aunty = () => {
      return (
           <div>
                <h2>Aunty</h2>
+               <section className='flex'>
+               <Cousin>Jamil</Cousin>
+               <Cousin>Jamila</Cousin>
+               </section>
           </div>
      );
 };
--- src/components/Father/Father.jsx
+++ src/components/Father/Father.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import MySelf from '../MySelf/MySelf';
+import Brother from '../Brother/Brother';
+import Sister from '../Sister/Sister';
 
 const Father = () => {
      return (
           <div>
                <h2>Father</h2>
+               <section className='flex'>
+                    <MySelf></MySelf>
+                    <Brother></Brother>
+                    <Sister></Sister>
+               </section>
           </div>
      );
 };
