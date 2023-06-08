84b42ba677a852aef8cefd626e9aa5a7021f5e98
mrfhmd7 <mrf.hmd7@gmail.com>
2023-06-08 13:44:47 +0600

    grandpa children added

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './components/Home/Home.jsx';
-import Main from './components/Layout/Main.jsx';
-import OrderReview from './components/OrderReview/OrderReview.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import Main from "./components/Layout/Main.jsx";
+import OrderReview from "./components/OrderReview/OrderReview.jsx";
+import Grandpa from "./components/Grandpa/Grandpa.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Main></Main>,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home></Home>,
-        loader: () => fetch('tshirts.json'),
+        loader: () => fetch("tshirts.json"),
       },
       {
-        path: 'review',
-        element: <OrderReview></OrderReview>
-      }
-    ]
-  }
-])
+        path: "review",
+        element: <OrderReview></OrderReview>,
+      },
+      {
+        path: "grandpa",
+        element: <Grandpa></Grandpa>,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
